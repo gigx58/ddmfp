afea48fc75a7b83e64723127f184dcd35360c31f
gigx58 <chicco.stranieri@libero.it>
2025-05-21 18:02:47 +0200

    itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,8 +13,8 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://gigx58-mazztvomg.hf.space"     # non mettere lo / finale al link
+MFPPSW = "58"
 
 
 # Constants
